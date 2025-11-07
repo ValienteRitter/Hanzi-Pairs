1c651b45f6f61562184e9599632dba36c5918bb3
ValienteRitter <tagizade2006f@gmail.com>
2025-11-07 15:17:05 +0400

    Feat: Add utility function that sets hanzi and pinyin in pairs

--- src/utilities.jsx
+++ src/utilities.jsx
@@ -1,1 +1,7 @@
-import hsk from './data/hsk.json'
+import hsk from "./data/hsk.json";
+
+export function setHanziPairs({ count, diffLevel }) {
+    return hsk
+        .filter(({ level }, index) => level === diffLevel && index < count)
+        .map(({ pinyin, hanzi }) => ({ pinyin: pinyin, hanzi: hanzi }));
+}

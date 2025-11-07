9ab8770b52c1dbce8c03fbc332a372d708a54110
ValienteRitter <tagizade2006f@gmail.com>
2025-11-07 18:21:47 +0400

    Feat: Create header for displaying score

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { HanziCardContainer } from "./components/HanziCardContainer";
 import { setHanziPairs } from "./utilities";
 import { PinyinCardContainer } from "./components/PinyinCardContainer";
+import { Header } from "./components/Header";
 
 import './App.css'
 
@@ -10,6 +11,7 @@ export function App() {
 
     return (
         <div className="app-container">
+            <Header />
             <HanziCardContainer data={hanziPairs} />
             <hr />
             <PinyinCardContainer data={hanziPairs} />
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export function Header() {
+    return (
+        <header>
+            <h1>Hanzi Pairs</h1>
+            <p>Score: 0</p>
+        </header>
+    )
+}
